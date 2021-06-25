2e91859792554f002178db7ea97a9181e00f3c84
+ <+>
2021-06-25 10:15:42 -0700

    commit tree 481ee87babf396058106fee7a406133851d67549

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create an Author
+author = Author.new(name: "S.E. Hinton", hometown: "Tulsa, OK")
+author.save
+
+# Create a Post
+post = Post.new(title: "The Outsiders", content: "The rivalry between two gangs, the poor Greasers and the rich Socs, only heats up when one gang member kills a member of the other.")
+post.author = author
+post.save
